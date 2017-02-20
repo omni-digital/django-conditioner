a12eaa39b71caba71cdfca7d564e3a621899a031
Paweł Adamczak <pawel.ad@gmail.com>
2017-02-20 11:19:18 +0000

    Added package metadata

--- conditioner/__init__.py
+++ conditioner/__init__.py
@@ -1,7 +1,13 @@
 """
 Conditioner
 ===========
-Create simple 'if this then that' style rules. Comes with a bunch of ready to use actions and conditions,
-but is also easily extensible and allows model specific actions/conditions.
+Create simple 'if this then that' style rules in your Django application. Comes with a bunch of ready to use actions
+and conditions, but is also easily extensible and allows model specific actions/conditions.
 """
 default_app_config = 'conditioner.apps.ConditionerAppConfig'
+
+
+__title__ = 'django-conditioner'
+__version__ = '0.1.0'
+__author__ = 'Omni Digital'
+__url__ = 'https://github.com/omni-digital/django-conditioner'
